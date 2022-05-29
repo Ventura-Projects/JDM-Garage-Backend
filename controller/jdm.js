8f824b38e90ab2385a0ea5d87c9b09fbcefaf48d
EXPITC <indratc18@gmail.com>
2022-05-30 00:31:22 +0700

    merge image api
    ger merge image api branch by expitc feature

--- controller/jdm.js
+++ controller/jdm.js
@@ -1,9 +1,13 @@
 // export schema
 const exampleDB = 'hello world'
-
+// export the cloudinary for uploading the Image
+const{ upToCloudinaryCars } = require('../utils/cloudinary');
+    
 const greet = async (req,res) => {
     try {
+        // retrieve data from databse
         const fromDb = await exampleDB
+        
         res.status(200).send({fromDb})
     } catch (err) {
         res.status(500).json(err);
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // const connectDB = require('./db/connect')
 const express = require('express');
 const router = require('./routes/Jdm') 
-    
-    
-require('dotenv').config()
 
 // port
 const port = 5000 || process.env.PORT;
@@ -19,7 +16,6 @@ const start = async () => {
         console.log('Starting server')
         // connect to monggoDB
         // await connectDB
-
 
         server.listen(port, console.log('server listening on port:'+port))
         console.log('connected')
