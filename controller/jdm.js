f02dc755d299db833fda9e1d0f579ced894b38f8
EXPITC <indratc18@gmail.com>
2022-05-30 00:34:44 +0700

    merge connected monggoDB
    merge conncted to monggoDB by expitc feature branch

--- controller/jdm.js
+++ controller/jdm.js
@@ -1,19 +1,46 @@
 // export schema
+const JDMCar = require('../schema/Jdm')
 const exampleDB = 'hello world'
-// export the cloudinary for uploading the Image
-const{ upToCloudinaryCars } = require('../utils/cloudinary');
-    
+
+// for capitalize first letter
+// const a = "hello".capitalize();
+Object.defineProperty(String.prototype, 'capitalize', {
+    value: function() {
+      return this.charAt(0).toUpperCase() + this.slice(1);
+    },
+    enumerable: false
+});
+
 const greet = async (req,res) => {
     try {
-        // retrieve data from databse
-        const fromDb = await exampleDB
-        
-        res.status(200).send({fromDb})
+        // const fromDb = await exampleDB
+        const JDMCars = await JDMCar.find();
+        res.status(200).json({JDMCars})
     } catch (err) {
         res.status(500).json(err);
     }
 }
+// const create = async (req, res) => {
+//     try {
+//         const JDM = await JDMCar.create({
+//             Type: 'AE86',
+//             Manufacturer: 'Toyota',
+//             Year: 1983,
+//             Image: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Sprinter_Trueno_1600_GT_%28AE86%29.jpg',
+//             EnginePosition: 'Front',
+//             Transmission: "Manual",
+//             WheelDrive: 'RWD',
+//             BodyStyle: '2-door',
+//             Acceleration0to62mph: 8.3,
+//             TopSpeedMPH:122
+//         })
+//         res.status.json({JDM})
+//     } catch (err) {
+//         res.status(500).json(err)
+//     }
+// }
 
 module.exports = {
-    greet
+    greet,
+    // create
 }
--- schema/jdm.js
+++ schema/jdm.js
@@ -1,2 +1,59 @@
 // make schema for monggoDB with mongoose | adjust schema to qualify the requirement criteria with project
 
+const mongoose = require('mongoose');
+
+const JDMSchema = new mongoose.Schema({
+    Type: {
+        type: String,
+        require: [true, 'Must Provide Type car!'],
+        trim: true,
+        maxlength:[20, 'Only 20 characters are allowed']
+    },
+    Manufacturer: {
+        type: String,
+        require: [true, 'Must Provide Manufacturer of that car!'],
+        trim: true
+    },
+    Year: {
+        type: Number,
+        require: [true, 'Must Provide Year of that car!'],
+        trim: true,
+        maxlength:[4, 'Only 4 digit years are allowed']
+    },
+    Image: {
+        type: String,
+        require: [true, 'Must Provide Image of that car!'],
+        trim:true
+    },
+    EnginePosition: {
+        type: String,
+        require: [true, 'Must Provide Engine Position of that car!'],
+        trim:true
+    },
+    Transmission: {
+        type: String,
+        require: [true, 'Must Provide Transmission of that car!'],
+        trim:true
+    },
+    WheelDrive: {
+        type: String,
+        require: [true, 'Must Provide Wheel Drive of that car!'],
+        trim:true,
+    },
+    BodyStyle: {
+        type: String,
+        require: [true, 'Must Provide Body Style of that car!'],
+        trim:true
+    },
+    Acceleration0to62mph: {
+        type: Number,
+        require: [true, 'Must Provide Acceleration 0 - 62mph of that car!'],
+    },
+    TopSpeedMPH: {
+        type: Number,
+        require: [true, 'Must Provide Top Speed MPH of that car!'],
+    },
+   
+
+},{ timestamps: true})
+module.exports= mongoose.model('JDMCar', JDMSchema);
